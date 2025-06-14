32b03a8615a1e260181fff981e20d64e6599b156
Tetyana Popenko <orkneya17@gmail.com>
2025-06-14 16:50:03 +0300

    Task4

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { getProductsController } from '../controllers/products.js';
+import {
+  getProductsController,
+  getProductByIdController,
+} from '../controllers/products.js';
 
 export const productsRouter = Router();
 productsRouter.get('/', ctrlWrapper(getProductsController));
+
+productsRouter.get('/:productId', ctrlWrapper(getProductByIdController));
--- src/services/products.js
+++ src/services/products.js
@@ -4,3 +4,13 @@ export const getAllProducts = async () => {
   const products = await Product.find();
   return products;
 };
+
+export const getProductById = async (productId) => {
+  console.log('====================================');
+  console.log(productId);
+  console.log('====================================');
+  const product = await Product.findOne({ _id: productId });
+  console.log(product);
+
+  return product;
+};
