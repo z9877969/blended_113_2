dfe7f6e985f8c3bf4630f8a103142af1d89f7dd6
z9877969 <z9877969@ukr.net>
2025-06-14 15:02:45 +0300

    resolves task-1

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 
 import { env } from './utils/env.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -13,9 +15,9 @@ export const setupServer = () => {
 
   //   app.use(productsRouter);
 
-  //   app.use('*', notFoundHandler);
+  app.use(notFoundHandler);
 
-  //   app.use(errorHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
